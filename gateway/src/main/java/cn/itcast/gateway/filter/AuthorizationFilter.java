1f24f2f1150a7ae493c1f45e20333a4e05eb0ca9
herunqiang <1584204039@qq.com>
2021-11-06 10:11:52 +0800

    gateway 集成

---
+++ gateway/src/main/java/cn/itcast/gateway/filter/AuthorizationFilter.java
@@ -0,0 +1,35 @@
+package cn.itcast.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+@Component
+//@Order(1)//数值与优先级成反比
+public class AuthorizationFilter implements GlobalFilter , Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        //获取请求参数authorization，如果为admin，则继续，否则终止
+        MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+        String authorization = queryParams.getFirst("authorization");
+        if ("admin".equals(authorization)) { //继续执行其他过滤器
+            return chain.filter(exchange);
+        }else{//直接响应
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@
 集群启动nacos :
     1.先启动每个nacos实例
     2.再启动Nginx代理每个nacos实例
+    注意：如果启动抛nacos异常导致获取不到nacos服务列表，删除一下nacos服务的data文件夹
